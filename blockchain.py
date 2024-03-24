de8473ac958b1dc29a39e438316191238f58d460
Josue Augusto <josueaugusto470@gmail.com>
2024-03-24 15:31:14 -0300

    initial commit

--- blockchain.py
+++ blockchain.py
@@ -3,19 +3,19 @@ import hashlib
 import json
 from flask import Flask, jsonify
 
-#Criar a blockchain
+
 class Blockchain:
-    def __init__(self): #Iniciando a classe
-        self.chain = [] #Iniciando lista 
-        self.create_block(data = 'Primeiro bloco',nonce = 1, previous_hash='0000000000000000000000000000000000000000000000000000000000000000' ) #Crinado o Genesis
+    def __init__(self): 
+        self.chain = [] 
+        self.create_block(data = 'Primeiro bloco',nonce = 1, previous_hash='0000000000000000000000000000000000000000000000000000000000000000' ) 
         
     def create_block(self,data, nonce, previous_hash):
         nonce_operation = self.proof_of_work(data, previous_hash)
         hash_operation = self.hash(data, nonce_operation, previous_hash)
-        block = {'index': len(self.chain)+ 1, #Tamanho da blockchain
+        block = {'index': len(self.chain)+ 1, 
              'timestamp': str(datetime.datetime.now()),
-             'nonce': nonce, #Pegando o nounse minerado
-             'previous_hash': previous_hash, #Hash anterior
+             'nonce': nonce, 
+             'previous_hash': previous_hash, 
              'data' : data,
              'hash_operation' : hash_operation
              } 
@@ -23,39 +23,39 @@ class Blockchain:
         return block
 
 
-    def get_previus_block(self): #Retornar o ultimo bloco
+    def get_previus_block(self): 
         return self.chain[-1]
 
-    def proof_of_work(self, data, previous_hash): # Validando o nouse
-        new_nonce = 1 #Nounse iniciado com 1
-        check_nonce = False # controle de validação
-        while check_nonce is False: #checando o os nounse
-            hash_opreation = str(self.hash(data, new_nonce, previous_hash)) #Gerando Hash em String
+    def proof_of_work(self, data, previous_hash): 
+        new_nonce = 1 
+        check_nonce = False 
+        while check_nonce is False: 
+            hash_opreation = str(self.hash(data, new_nonce, previous_hash)) 
             print(hash_opreation)
-            if hash_opreation[:3] == '000': #Validadndo o Hash gerado 
+            if hash_opreation[:3] == '000': 
                check_nonce = True
             else:
                new_nonce +=1
         return new_nonce
     
-    def nounce_check(self, data, nonce, previous_hash): # Validando o nouse
-        check_nonce = False # controle de validação
-        hash_opreation = str(self.hash(data, nonce, previous_hash))#Gerando Hash em String
-        if hash_opreation[:3] == '000': #Validadndo o Hash gerado 
+    def nounce_check(self, data, nonce, previous_hash): 
+        check_nonce = False 
+        hash_opreation = str(self.hash(data, nonce, previous_hash))
+        if hash_opreation[:3] == '000': 
            check_nonce = True
             
         return check_nonce
 
-    def hash(self, data, new_nonce, previous_hash): #Criar Hash com Bloco em contrução
+    def hash(self, data, new_nonce, previous_hash): 
         block = {   
                     'nonce': new_nonce,
                     'previous_hash': previous_hash,
                     'data': data
                     }
-        encode_hash = json.dumps(block, sort_keys=True).encode() #Transformando o bloco em Jason
-        return hashlib.sha256(encode_hash).hexdigest() #Gerando Hash com bloco Jason
+        encode_hash = json.dumps(block, sort_keys=True).encode() 
+        return hashlib.sha256(encode_hash).hexdigest() 
 
-    def is_chain_valid(self, chain): #Percorrer a blockchain para validar todos os Hash's
+    def is_chain_valid(self, chain): 
         previus_block = chain[0]
         block_index = 1
         while block_index < len(chain):
@@ -69,18 +69,18 @@ class Blockchain:
             block_index += 1
         return True
 
-app = Flask(__name__) #Inicializando Flask
+app = Flask(__name__) 
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
-Blockchain = Blockchain() #Instanciando a classe blockchain
+Blockchain = Blockchain() 
 
 
 @app.route('/mine_block', methods=['GET'])
 def mine_block():
     data =' teste de bloco: '
-    previus_block = Blockchain.get_previus_block() # Pegando o ultimo bloco
+    previus_block = Blockchain.get_previus_block() 
     previous_hash = previus_block['hash_operation']
-    gold_nonce = Blockchain.proof_of_work(data, previous_hash) #validando o Nounse
+    gold_nonce = Blockchain.proof_of_work(data, previous_hash) 
     block = Blockchain.create_block(data,gold_nonce, previous_hash)
     response = {'message': 'Bloco minerado!',
                 'index' : block['index'],
@@ -92,7 +92,7 @@ def mine_block():
     return jsonify(response), 200
 
 
-@app.route('/get_chain', methods=['GET'])#retornando toda a blockchain
+@app.route('/get_chain', methods=['GET'])
 def get_chain():
     response = {'chain': Blockchain.chain, 
                 'length':len(Blockchain.chain)}
